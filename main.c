a82bba0df29c338aeda8c02638be5aa911f51594
Повелитель МаКаРоН <82654703+Topalo-Denus@users.noreply.github.com>
2021-04-27 00:18:41 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <math.h>
 int main(void) {
-
 printf("\nt = log2((1-y)/1+x))+z");//4 laba 1 exercise 22 option
 
 float t,num,x,y,z;
@@ -9,23 +8,24 @@ metka:
 
 printf ("\n\nedit x=");
 scanf("%f",&x);
+
 printf ("\nedit y=");
 scanf("%f",&y);
+
 printf ("\nedit z=");
 scanf("%f",&z);
 
-if (x==-1)
-printf ("can't share on 0,try again");
-if(x==-1)
+while (x==-1){
+printf ("\ncan't share on 0,try again");
 goto metka;
+}
 
 num=((1-y)/(1+x))+z;
 
-if(num<0)
+while (num<0){
 printf ("\ncan't be negative number in logarithm,try again");
-if(num<0)
 goto metka;
-
+}
 
 t=log2(num);
 printf ("\nt=%f",t);
